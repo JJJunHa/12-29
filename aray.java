5174efff08fe0087b53f7e749e21ad81366324a1
JJJunHa <121237261+JJJunHa@users.noreply.github.com>
2022-12-30 17:11:09 +0900

    Add files via upload

---
+++ aray.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class aray {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner (System.in);
+		
+		int[] m = new int[5];
+		int i=0;
+		int sum=0;
+		int pp=0;
+		while(i<m.length) {
+			System.out.println("입력하시오");
+			m[i]=s.nextInt();
+			sum+=(m[i]);
+			i++;
+		}
+		
+		System.out.println("------------------");
+		i=0;
+		
+		while(i<m.length) {
+			//점수의 합계를 누적
+			System.out.println(m[i]);
+			
+			i++;
+		}
+		System.out.println("총합 "+sum);
+		System.out.println("평균은 "+(sum/(m.length)));
+		
+		//평균점수 계산
+		System.out.println("프로그램종료.");
+	}
+
+}

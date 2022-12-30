fe3c2173fde2a2c87f2c31776b187fb1b49406f0
JJJunHa <121237261+JJJunHa@users.noreply.github.com>
2022-12-30 16:45:43 +0900

    Add files via upload
    총합구하기

---
+++ test1.java
@@ -0,0 +1,14 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		int x = 0;
+		int total = 0; 
+		while(x<=100) {
+			total+=x; 
+		x++;}
+		
+System.out.println("1부터 100까지의 총합은 "+total+" 입니다.");
+	}
+
+}

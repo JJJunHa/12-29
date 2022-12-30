5553db2d7d0f48baf2792fe4824c672fd4ae6fa2
JJJunHa <121237261+JJJunHa@users.noreply.github.com>
2022-12-30 16:43:37 +0900

    Add files via upload

---
+++ works.java
@@ -0,0 +1,23 @@
+
+public class works {
+
+	public static void main(String[] args) {
+		
+		 int i = 0;
+		 
+		while(i<=19) {
+			i++;
+			boolean 
+			x = i%2==1;
+			
+			if(x) {
+			
+				System.out.println(i);
+				
+			
+		}
+
+	}
+
+}
+}

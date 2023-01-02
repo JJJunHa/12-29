21dd64c42e1ee719b5eaa6cca6b42ab3249a9687
JJJunHa <121237261+JJJunHa@users.noreply.github.com>
2023-01-02 14:05:47 +0900

    Add files via upload

---
+++ Exercise07.java
@@ -0,0 +1,20 @@
+
+public class Exercise07 {
+
+	public static void main(String[] args) {
+		int max =0;
+		int[] array = { 1, 9, 3, 8,};
+		
+		for(int i = 0; i<array.length; i++) {
+			
+			if(max<array[i]) {
+				max = array[i]; 
+			}
+		}
+		
+		System.out.println("최대값은 " + max);
+		
+
+	}
+
+}

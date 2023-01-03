f9b85c6d5b09cebf8d440a3927119020bdcb9d23
JJJunHa <121237261+JJJunHa@users.noreply.github.com>
2023-01-03 14:53:20 +0900

    Add files via upload

---
+++ work1.java
@@ -0,0 +1,77 @@
+
+public class work1 {
+
+	public static void main(String[] args) {
+		
+		
+	
+//		for(; c<1000;) {
+//			c=a+b;
+//			if(c<1000) {
+//			System.out.print("a= "+a+" ");
+//			System.out.print("b= "+b+" ");
+//			System.out.print("c= "+c+" ");
+//			}
+//			
+//			a=b;
+//			b=c;
+//			System.out.println("");
+//				
+//		}
+		
+//		while(c<1000) {
+//			c=a+b;
+//			if(c<1000) {
+//				System.out.print("a= "+a+" ");
+//				System.out.print("b= "+b+" ");
+//				System.out.print("c= "+c+" ");
+//				}
+//			
+//			a=b;
+//			b=c;
+//			System.out.println("");
+//			
+//				
+//			
+//		}
+		int count=0;
+		int j = 0;
+		int i = 0;
+//		for(i = 2; i<20; i++) {
+//			for(j = 2; j<=i; j++) {
+//				
+//				
+//				if(i%j==0) {
+//					System.out.println(i);
+//					break;
+//				}
+//				
+//			}if(j==i);
+//			
+//		}
+//		System.out.println(count);
+		
+		for(i=1; i<20; i++ ) {
+	         for(j=2; j<i; j++) {
+	            boolean a = (i%j==0);
+	            
+	            
+	                if(a) {
+	                   break;
+	               }
+	                
+	            }if(j==i) {
+	            	System.out.println(i);
+		        	  
+		         }
+	         
+	         }
+	
+	}}
+			 
+			 
+		 
+		
+		 
+	
+

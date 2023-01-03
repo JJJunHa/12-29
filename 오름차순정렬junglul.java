abbe04f7bd5d70a85a1aeeda791e2f91db57834f
JJJunHa <121237261+JJJunHa@users.noreply.github.com>
2023-01-03 16:25:13 +0900

    Update 오름차순정렬junglul.java

--- 오름차순정렬junglul.java
+++ 오름차순정렬junglul.java
@@ -21,6 +21,19 @@ public class junglul {
 				System.out.print(x[i]+" ");
 			}
 			
+	
+		for(i=0; i<x.length-1; i++) {
+			for(j=i+1; j<x.length; j++) {
+				if(x[i]<x[j]) {
+					temp=x[i];
+					x[i]=x[j];
+					x[j]=temp;	
+				}
+			}
+		}
+			for(i = 0; i<x.length; i++) {
+				System.out.print(x[i]+" ");
+			}
 		}
 	}
 

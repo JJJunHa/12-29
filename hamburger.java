a96794b198dabcf67d295b9cb24b4ef73baf6fe0
JJJunHa <121237261+JJJunHa@users.noreply.github.com>
2023-02-16 17:25:16 +0900

    Add files via upload

--- hamburger.java
+++ hamburger.java
@@ -1,45 +1,151 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class hamburger {
 
 	public static void main(String[] args) {
 		int answer=0;
-		String notthing="";
-		
-		boolean bread=false;
-		boolean vegetables=false;
-		boolean meat=false;
-		boolean bread2=false;
-		int[]ingredient= {1,1,2,2,3,1,2,1,1,2,1,2,3,1,3,1,3,1,1,2,3,1,1,1,2,3,1,2,3,1};
-//		2, , 2,3, 1,1
+		int[]ingredient= {1,1,2,3,1,1,2,3,1};
+		System.out.println(ingredient.length);
 		int i=0;
-		
+		int check=0;
 //		if(ingredient.length==4) {
-//			
 //			if(ingredient[i]==1&&ingredient[i+1]==2&&ingredient[i+2]==3&&ingredient[i+3]==1) {
 //				answer++;
 //			}
 //		}
-		while(i<ingredient.length-3) {
-			
+		while(i<ingredient.length) {
+			System.out.println(i);
+			System.out.print(ingredient[i]);
+			System.out.print(ingredient[i+1]);
+			System.out.print(ingredient[i+2]);
+			System.out.print(ingredient[i+3]);
+			System.out.println("--------");
 			if(ingredient[i]==1&&ingredient[i+1]==2&&ingredient[i+2]==3&&ingredient[i+3]==1) {
 				answer++;
 				if((i-1)>=0) {
 					ingredient[i+3]=ingredient[i-1];
+					check=1;
 				}
 				if((i-2)>=0) {
+					check=2;
 					ingredient[i+2]=ingredient[i-2];
 				}
 				if((i-3)>=0) {
 					ingredient[i+1]=ingredient[i-3];
 				}
 				if((i-4)>=0) {
+					check=3;
 					ingredient[i]=ingredient[i-4];
+				}
+				if(check==1) {
+					i=i+2;
+				}
+				else if(check==2) {
+					i=i+1;
+				}
+				else if(check==3) {
+					i=i-1;
 				}
 			}
 			i++;
 			
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+//		int bread=0; -------------어레이 리스트 정답
+//		int vegetables=0;
+//		int meat=0;
+//		int bread2=0;
+//		int steck=0;
+//		int store=0;
+//		int[]ingredient= {2, 1, 1, 2, 3, 1, 2, 3, 1};
+//		String sivar=Arrays.toString(ingredient);
+//		System.out.println(sivar);
+//		sivar.contains("1, 2, 3, 1");
+//		String notthing="";
+//		int i=0;
+//		int j=0;
+//		ArrayList<Integer> m =new ArrayList<Integer>();
+//		if(sivar.contains("1, 2, 3, 1")) {
+//		for(j=0; j<ingredient.length; j++) {
+//			m.add(ingredient[j]);
+//			notthing = m.toString();
+//			if(notthing.contains("1, 2, 3, 1")) {
+//				answer++;
+//				if(i>=0) {
+//				m.remove(i);
+//				}
+//				if(i-1>=0) {
+//				m.remove(i-1);
+//				}
+//				if(i-2>=0) {
+//				m.remove(i-2);
+//				}
+//				if(i-3>=0) {
+//				m.remove(i-3);
+//				i=i-4;
+//				}
+//			}
+//			i++;
+//		}
+//		}
+		
+		
+		
+		
+		
+//		for(i=0; i<ingredient.length; i++) {
+//			if(i+4<ingredient.length) {
+//			if(ingredient[i]==1&&ingredient[i+1]==2&&ingredient[i+2]==3&&ingredient[i+3]==1) {
+//				i=i+4;
+//				answer++;
+//			 }
+//			}
+//			m.add(ingredient[i]);
+//		}
+//		for(i=0; i<m.size()-3;i++) {
+//			if(m.get(i)==1&&m.get(i+1)==2&&m.get(i+2)==3&&m.get(i+3)==1) {
+//				answer++;
+//				m.remove(i);
+//				m.remove(i);
+//				m.remove(i);
+//				m.remove(i);
+//				
+//			}
+//		}
+//		if(ingredient.length==4) {
+//			
+//			if(ingredient[i]==1&&ingredient[i+1]==2&&ingredient[i+2]==3&&ingredient[i+3]==1) {
+//				answer++;
+//			}
+//		}
+//		while(i<ingredient.length-3) {
+//			
+//			if(ingredient[i]==1&&ingredient[i+1]==2&&ingredient[i+2]==3&&ingredient[i+3]==1) {
+//				answer++;
+//				if((i-1)>=0) {
+//					ingredient[i+3]=ingredient[i-1];
+//				}
+//				if((i-2)>=0) {
+//					ingredient[i+2]=ingredient[i-2];
+//				}
+//				if((i-3)>=0) {
+//					ingredient[i+1]=ingredient[i-3];
+//				}
+//				if((i-4)>=0) {
+//					ingredient[i]=ingredient[i-4];
+//				}
+//			}
+//			i++;
+//			
+//		}
 //        for(int i = 0; i < ingredient.length - 3; i++) {
 //            if(ingredient[i] == 1 && ingredient[i+1] == 2 && ingredient[i+2] == 3 && ingredient[i+3] == 1) {
 //                answer++;
@@ -173,7 +279,7 @@ public class hamburger {
 //		
 ////		System.out.println(notthing2);
 //		}
-		System.out.println(answer);
+		System.out.println("ANSWER="+answer);
 		
 		//----------------------------------------------
 //		if(notthing.contains("1231")) {
